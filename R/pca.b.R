661cf2416faf6460d27898e0a9396f7e935b9fd7
hyunsooseol <snow@cau.ac.kr>
2020-11-08 11:34:41 +0900

    Adding PCA Plot

---
+++ R/pca.b.R
@@ -0,0 +1,110 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+#' @importFrom factoextra fviz_pca_var
+#' @importFrom factoextra fviz_pca_ind
+#' @importFrom factoextra fviz_pca_biplot
+#' @importFrom FactoMineR PCA
+#' @import ggplot2
+#' @export
+
+
+pcaClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "pcaClass",
+    inherit = pcaBase,
+    private = list(
+        .run = function() {
+
+            
+            if (!is.null(self$options$vars)) {
+                
+                vars <- self$options$vars
+                
+                data <- self$data
+                
+                data <- jmvcore::naOmit(data)
+                
+                for (i in seq_along(vars))
+                    data[[i]] <- jmvcore::toNumeric(data[[i]])
+                
+         
+          #   rownames(data)<-data[,1]
+            
+            
+          # principal component analysis---------
+           
+            pca <- FactoMineR::PCA(data,  graph = FALSE)
+            
+      
+            # Variable contributions plot----------
+
+            image <- self$results$plot
+            image$setState(pca)
+            
+            # Individual plot-------
+            
+            
+            image1 <- self$results$plot1
+            image1$setState(pca)
+            
+
+            # Biplot--------
+            
+            image2 <- self$results$plot2
+            image2$setState(pca)
+            
+            }
+
+        },
+
+        # Control variable colors using their contributions----------
+
+        .plot = function(image, ggtheme, theme, ...) {
+
+            if (length(self$options$vars) < 2)
+                return()
+
+            pca <- image$state
+
+            plot <- factoextra::fviz_pca_var(pca, col.var="contrib",
+                                              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                              repel = TRUE # Avoid text overlapping
+            )
+
+            print(plot)
+            TRUE
+        },
+       
+       
+       .plot1 = function(image1, ggtheme, theme, ...) {
+           
+           if (length(self$options$vars) < 2)
+               return()
+           
+           pca <- image1$state
+           
+           plot1 <- factoextra::fviz_pca_ind(pca, col.ind = "cos2",
+                                            gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                            repel = TRUE # Avoid text overlapping (slow if many points)
+           )
+           
+           print(plot1)
+           TRUE
+       },
+       
+       .plot2 = function(image2, ggtheme, theme, ...) {
+           
+           if (length(self$options$vars) < 2)
+               return()
+           
+           pca <- image2$state
+           
+           plot2 <- factoextra::fviz_pca_biplot(pca, repel = TRUE)
+           
+           print(plot2)
+           TRUE
+       }
+       
+
+        )
+)
