9513c41bce72cf477fe300b9dab9b13507a3d88d
hyunsooseol <snow@cau.ac.kr>
2023-06-09 19:37:43 +0900

    update version

--- R/00jmv.R
+++ R/00jmv.R
@@ -7,7 +7,7 @@
         `author`="Seol, H.", 
         `year`=2023, 
         `title`="snowCluster: Multivariate Analysis", 
-        `publisher`="(Version 7.1.8)[jamovi module]. URL https://github.com/hyunsooseol/snowCluster", 
+        `publisher`="(Version 7.1.9)[jamovi module]. URL https://github.com/hyunsooseol/snowCluster", 
         `url`="https://github.com/hyunsooseol/snowCluster"), 
     `factoextra`=list(
         `type`="software", 
