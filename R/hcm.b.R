37c02e27eb4282e1c34f16b1ced5a10136b57dd5
hyunsooseol <snow@cau.ac.kr>
2025-07-26 23:19:29 +0900

    fixed hcm

--- R/hcm.b.R
+++ R/hcm.b.R
@@ -14,7 +14,6 @@ hcmClass <- if (requireNamespace('jmvcore'))
         if (is.null(self$data) |
             is.null(self$options$podatki)) {
           self$results$instructions$setVisible(visible = TRUE)
-          
         }
         
         self$results$instructions$setContent(private$.htmlwidget$generate_accordion(
@@ -24,20 +23,17 @@ hcmClass <- if (requireNamespace('jmvcore'))
             '<div style="text-align:justify;">',
             '<ul>',
             '<li>The hierarchical clustering module was created by Gasper Cankar & Hyunsoo Seol.</li>',
+            '<li>Missing values in clustering variables will be omitted, and corresponding cases will show NA in the results.</li>',
             '<li>Feature requests and bug reports can be made on my <a href="https://github.com/hyunsooseol/snowCluster/issues" target="_blank">GitHub</a>.</li>',
             '</ul></div></div>'
-            
           )
-          
         ))
-        
         
         if (self$options$dend == TRUE)
           self$results$plot$setVisible(visible = TRUE)
         if (self$options$heat == TRUE)
           self$results$heat$setVisible(visible = TRUE)
-        if ((self$options$pair == TRUE) &
-            (self$options$case == FALSE))
+        if ((self$options$pair == TRUE) & (self$options$case == FALSE))
           self$results$pairs$setVisible(visible = TRUE)
         
         if (isTRUE(self$options$dend)) {
@@ -45,23 +41,19 @@ hcmClass <- if (requireNamespace('jmvcore'))
           height <- self$options$height
           self$results$plot$setSize(width, height)
         }
-        
         if (isTRUE(self$options$heat)) {
           width <- self$options$width1
           height <- self$options$height1
           self$results$heat$setSize(width, height)
         }
-        
         if (isTRUE(self$options$pair)) {
           width <- self$options$width2
           height <- self$options$height2
           self$results$pairs$setSize(width, height)
         }
-        
       },
       
       .run = function() {
-        # check if there are at least 3 variables to cluster with
         if (length(self$options$podatki) < 3)
           return()
         
@@ -73,41 +65,52 @@ hcmClass <- if (requireNamespace('jmvcore'))
         group <- self$options$group
         dis <- self$options$dis
         method <- self$options$method
-        group <- self$options$group
         dend <- self$options$dend
         heat <- self$options$heat
         horiz <- self$options$horiz
         hght <- self$options$hght
         
-        # if we have variable for labels do it with labels...
-        # scaling the data at the same time...na.omit takes care of NA values...
+        data <- self$data
+        podatki <- self$options$podatki
+        
+        # 결측이 없는 행 인덱스 추출
+        complete_idx <- which(stats::complete.cases(data[, podatki, drop = FALSE]))
+        n_row <- nrow(data)
+        
+        # (1) 군집분석 데이터 생성
         if (length(imena) > 0) {
-          ime <- as.character(self$data[, which(names(self$data) == imena)])
-          pod0 <- self$data[, which(names(self$data) != imena)]
+          ime <- as.character(data[, which(names(data) == imena)])
+          pod0 <- data[, which(names(data) != imena)]
           if (stand == TRUE) {
             pod <- scale(na.omit(pod0))
           } else {
             pod <- na.omit(pod0)
           }
-          rownames(pod) <- ime
+          rownames(pod) <- ime[complete_idx]
         } else {
           if (stand == TRUE) {
-            pod <- scale(na.omit(self$data))
+            pod <- scale(na.omit(data[, podatki, drop = FALSE]))
           } else {
-            pod <- na.omit(self$data)
+            pod <- na.omit(data[, podatki, drop = FALSE])
           }
         }
-        # change cases/variables if needed
+        
+        # case 옵션 처리
         if (case)
           pod <- t(pod)
-        # calculate distances
-        # pod is scaled dataset
-        razdalje <- stats::dist(pod, method = dis)
         
-        # clustering
+        # (2) 거리 계산 및 군집분석
+        razdalje <- stats::dist(pod, method = dis)
         klastri <- stats::hclust(d = jmvcore::toNumeric(razdalje), method = method)
         
-        # populate the table izpis
+        # (3) 클러스터 잘라내기
+        if (grp == "height") {
+          razrez <- cutree(klastri, h = hght)
+        } else {
+          razrez <- cutree(klastri, k = group)
+        }
+        
+        # (4) 결과 테이블 populate
         table <- self$results$izpis
         table$setRow(
           rowNo = 1,
@@ -118,72 +121,56 @@ hcmClass <- if (requireNamespace('jmvcore'))
             method = method
           )
         )
-        # populate the table Group membership
+        
+        # (5) 군집 빈도수 테이블
         table2 <- self$results$groups
-        
-        #cut the clusters
-        if (grp == "height") {
-          razrez <- cutree(klastri, h = hght)
-        } else {
-          razrez <- cutree(klastri, k = group)
-        }
-        
         tab <- as.data.frame(table(razrez))
-        
         for (i in 1:nrow(tab)) {
           table2$addRow(rowKey = i,
                         values = list(cluster = as.numeric(as.character(tab[i, 1])), freq = tab[i, 2]))
         }
         
-        #############################################
-        # Cluster membership-------------
-        
-        m <- as.data.frame(razrez)
-        mem <- m[, 1]
-        
+        # (6) **군집 membership: 결측 매칭**
+        mem <- rep(NA, n_row)
+        mem[complete_idx] <- as.numeric(razrez)
         self$results$clust$setValues(mem)
-        
         self$results$clust$setRowNums(rownames(data))
-        #############################
         
-        
-        #providing plot
+        # (7) Dendrogram plot
         if (dend == TRUE) {
           image <- self$results$plot
-          
           image$setState(list(klastri, razrez))
         }
         
-        #providing heatmap
+        # (8) Heatmap plot
         if (heat == TRUE) {
           image2 <- self$results$heat
           image2$setState(pod)
         }
         
-        #providing pairs plot
+        # (9) Pairs plot
         if ((pair == TRUE) & (case == FALSE)) {
           image3 <- self$results$pairs
-          image3$setState(cbind(pod, "cluster" = razrez))
+          # pair plot: NA매칭된 원본 데이터로 전달
+          tmp <- matrix(NA, nrow=n_row, ncol=length(podatki)+1)
+          colnames(tmp) <- c(podatki, "cluster")
+          tmp[complete_idx, ] <- cbind(pod, razrez)
+          image3$setState(tmp)
         }
-        
-        
-        #small textual output
-        #self$results$text$setContent(pod[1:3,])
+        # (10) 텍스트 결과 필요 시
+        # self$results$text$setContent(pod[1:3,])
       },
+      
       .plot = function(image, ...) {
         if ((length(self$options$podatki) < 3) ||
             (self$options$dend == FALSE))
           return()
-        
         plotData <- image$state[[1]]
         razrez <- image$state[[2]]
-        
-        #draw the dendrogram
         if (self$options$horiz == TRUE) {
           plott <- plot(stats::as.dendrogram(plotData),
                         type = "rectangle",
                         horiz = TRUE)
-          #if(self$options$grp == "height"){stats::rect.hclust(plotData,h=self$options$hght,cluster=razrez)} else {stats::rect.hclust(plotData,k=self$options$group,cluster=razrez)}
         } else {
           plott <- plot(stats::as.dendrogram(plotData),
                         hang = -1,
@@ -198,9 +185,9 @@ hcmClass <- if (requireNamespace('jmvcore'))
                                cluster = razrez)
           }
         }
-        
         TRUE
       },
+      
       .heat = function(image2, ...) {
         if ((length(self$options$podatki) < 3) ||
             (self$options$heat == FALSE))
@@ -208,23 +195,31 @@ hcmClass <- if (requireNamespace('jmvcore'))
         old <- par(mar = c(7, 5, 5, 3) + .01)
         plotData <- image2$state
         if (self$options$heat == TRUE) {
-          #draw the heatmap
           stats::heatmap(plotData)
         }
         par(old)
         TRUE
       },
+      
       .pairs = function(image3, ...) {
         if ((length(self$options$podatki) < 3) ||
             (self$options$pair == FALSE))
           return()
         plotData <- image3$state
+        # plotData가 NA로만 이루어진 경우 예외처리
+        if (all(is.na(plotData))) {
+          warning("All rows are NA after removing missing values. Pair plot will not be displayed.")
+          return(FALSE)
+        }
+        # 마지막 열이 cluster 번호임
         cluster <- as.numeric(plotData[, ncol(plotData)])
-        plotData <- plotData[, -ncol(plotData)]
-        #draw pairs plot
-        graphics::pairs(plotData,
+        plotData2 <- plotData[, -ncol(plotData), drop=FALSE]
+        # NA 행 제거 후 pairs plot
+        complete_idx <- which(stats::complete.cases(plotData2))
+        if (length(complete_idx) < 2) return(FALSE)
+        graphics::pairs(plotData2[complete_idx, , drop=FALSE],
                         pch = 21,
-                        bg = grDevices::heat.colors(self$options$group)[cluster])
+                        bg = grDevices::heat.colors(self$options$group)[cluster[complete_idx]])
         TRUE
       }
     )
