52dbfdb92755abc596effb3f979efaddfab311d0
hyunsooseol <snow@cau.ac.kr>
2025-07-26 23:55:45 +0900

    fixed missing values in time clust analysis

--- R/timeclust.b.R
+++ R/timeclust.b.R
@@ -1,5 +1,6 @@
 # This file is a generated template, your changes will not be overwritten
 #' @importFrom magrittr %>% 
+
 timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
   R6::R6Class(
     "timeclustClass",
@@ -10,10 +11,8 @@ timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
       
       .init = function() {
         private$.htmlwidget <- HTMLWidget$new()
-        
         if (is.null(self$data) | is.null(self$options$item)) {
           self$results$instructions$setVisible(visible = TRUE)
-          
         }
         self$results$instructions$setContent(private$.htmlwidget$generate_accordion(
           title = "Instructions",
@@ -25,25 +24,18 @@ timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
             '<li>The variables must be named <b>time, item, and value</b> respectively.</li>',
             '<li>Feature requests and bug reports can be made on my <a href="https://github.com/hyunsooseol/snowCluster/issues" target="_blank">GitHub</a>.</li>',
             '</ul></div></div>'
-            
           )
-          
         ))
-        
         if (isTRUE(self$options$plot)) {
           width <- self$options$width
           height <- self$options$height
-          
           self$results$plot$setSize(width, height)
         }
-        
         if (isTRUE(self$options$plot1)) {
           width <- self$options$width1
           height <- self$options$height1
-          
           self$results$plot1$setSize(width, height)
         }
-        
       },
       
       .run = function() {
@@ -52,88 +44,45 @@ timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
             is.null(self$options$feature) | is.null(self$options$value))
           return()
         
-        
-        # item<- self$options$item
-        # feature<- self$options$feature
-        # value<- self$options$value
-        # k <- self$options$k
-        #
-        # data <- self$data
-        # data <- jmvcore::naOmit(data)
-        # data <- as.data.frame(data)
-        #
-        #
-        # data[,self$options$feature] <- as.Date(data[,self$options$feature])
-        #
-        # data <- tibble::as.tibble(data)
-        
-        #self$results$text$setContent(data)
-        
-        # km<- function (tbl, item, feature, value, k, fill = 0, ...)
-        # {
-        #   item_str <- as.character(substitute(item))
-        #   feature_str <- as.character(substitute(feature))
-        #   value_str <- as.character(substitute(value))
-        #   form <- stats::as.formula(paste(item_str, "~", feature_str))
-        #   m <- tbl %>% reshape2::acast(form, value.var = value_str,
-        #                                fill = fill)
-        #
-        #   clustered <- stats::kmeans(m, k, ...)
-        #   i <- match(rownames(m), as.character(tbl[[item_str]]))
-        #   tibble::tibble(`:=`(!!sym(item_str), tbl[[item_str]][i]),
-        #                  cluster = factor(clustered$cluster)) %>%
-        #                  dplyr::arrange(cluster)
-        # }
-        
-        #all <- private$.computeRES()
-        
+        # 결과 캐시
         if (is.null(private$.allCache)) {
           private$.allCache <- private$.computeRES()
         }
-
         all <- private$.allCache
-
+        
         if (isTRUE(self$options$plot1)) {
           image <- self$results$plot1
           image$setState(all$bic)
         }
         
-        # cluster number--
-        
+        # 군집 결과값 (결측 매칭)
         if (isTRUE(self$options$clust)) {
-          m <- as.factor(all$df$cluster)
-          
-          if (self$options$clust
-              && self$results$clust$isNotFilled()) {
+          m <- all$cluster_vec
+          if (self$options$clust && self$results$clust$isNotFilled()) {
             self$results$clust$setValues(m)
-            self$results$clust$setRowNums(rownames(data))
+            self$results$clust$setRowNums(rownames(self$data))
           }
-          
         }
         
         if (isTRUE(self$options$plot)) {
           image <- self$results$plot
           image$setState(all$df)
         }
-        
       },
       
       # plot---
-      
       .plot1 = function(image, ...) {
         if (is.null(image$state))
           return(FALSE)
-        
         bic <- image$state
         plot1 <- plot(bic)
         print(plot1)
         TRUE
       },
-
+      
       .plot = function(image, ggtheme, theme, ...) {
         if (is.null(image$state))
           return(FALSE)
-        
         df <- image$state
         library(ggplot2)
         plot <- ggplot(df, aes(
@@ -146,11 +95,9 @@ timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
           scale_y_continuous(labels = scales::comma) +
           facet_wrap(vars(cluster)) +
           ggthemes::scale_color_solarized()
-        
         plot <- plot + ggtheme
         print(plot)
         TRUE
-        
       },
       
       .computeRES = function() {
@@ -160,40 +107,51 @@ timeclustClass <- if (requireNamespace('jmvcore', quietly = TRUE))
         k <- self$options$k
         
         data <- self$data
-        data <- jmvcore::naOmit(data)
-        data <- as.data.frame(data)
+        na_vars <- c(item, feature, value)
+        complete_idx <- which(stats::complete.cases(data[, na_vars, drop=FALSE]))
+        data_noNA <- data[complete_idx, , drop=FALSE]
         
-        is.Date <- function(x) {
-          inherits(x, "Date")
+        is.Date <- function(x) inherits(x, "Date")
+        if (!is.Date(data_noNA[, feature])) {
+          data_noNA[, feature] <- as.Date(paste(data_noNA[, feature], 1, 1, sep = "-"), format = "%Y-%m-%d")
         }
+        data_tbl <- tibble::as_tibble(data_noNA)
         
-        # Step 1: transform year into Date---
-        if (!is.Date(data[, self$options$feature])) {
-          data[, self$options$feature] <- as.Date(paste(data[, self$options$feature], 1, 1, sep = "-"), format = "%Y-%m-%d")
-        } else {
-          data[, self$options$feature] <- data[, self$options$feature]
+        # --- [여기서 k가 distinct item 개수보다 크면 에러!] ---
+        n_items <- length(unique(data_noNA[[item]]))
+        if (k > n_items) {
+          stop(paste0(
+            "Number of clusters (k=", k, ") must be less than or equal to number of distinct items (", n_items, ") after removing missing values."
+          ))
         }
+        # -----------------------------------------------------
         
-        data <- tibble::as.tibble(data)
-        # Optimal number of clusters using BIC---
-        bic <- mclust::mclustBIC(data$value)
+        # BIC 계산
+        bic <- mclust::mclustBIC(data_noNA[[value]])
         self$results$text$setContent(bic)
-        # Perform k-means clustering using widely_kmeans
-        # The variable names should be item, time, value respectively!!!
+        
+        # kmeans clustering (여기가 핵심)
         set.seed(1234)
-        res <-  data %>%
-          widyr::widely_kmeans(
-            tbl = data,
-            item = item,
-            feature = time,
-            value = value,
-            k = k
-          )
+        res <- widyr::widely_kmeans(
+          tbl = data_tbl,
+          item = item,        # 변수명 문자열!
+          feature = feature,
+          value = value,
+          k = k
+        )
         
-        df <- dplyr::left_join(data, res)
-        retlist = list(res = res, df = df, bic = bic)
+        df <- dplyr::left_join(data_tbl, res)
+        cluster_vec <- rep(NA, nrow(data))
+        cluster_vec[complete_idx] <- as.character(res$cluster)
+        
+        retlist = list(
+          res = res,
+          df = df,
+          bic = bic,
+          cluster_vec = cluster_vec,
+          complete_idx = complete_idx
+        )
         return(retlist)
-        
       }
     )
   )
